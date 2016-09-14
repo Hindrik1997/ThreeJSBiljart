146ab3191d0760b6fdefd39ec80e45e5472e5055
Jorn van Wier <jornvanwier@gmail.com>
2016-09-14 15:29:40 +0200

    123w

--- script/physics/physicsobject.js
+++ script/physics/physicsobject.js
@@ -2,6 +2,7 @@ class PhysicsObject extends GameObject {
     constructor(mesh, geometry, material, isMovable, boundingBox) {
         super(mesh, geometry, material);
         this._isMovable = isMovable;
+        this._boundingBox = boundingBox;
     }
 
     get isMoveable() {
@@ -11,4 +12,19 @@ class PhysicsObject extends GameObject {
     set isMoveable(value) {
         this._isMovable = value;
     }
+
+    get boundingBox() {
+        return this._boundingBox;
+    }
+
+    set boundingBox(value) {
+        this._boundingBox = value;
+    }
+
+    isCollidingWith(otherObject)
+    {
+        if(this.boundingBox instanceof THREE.)
+    }
+
+    isSphereCollidingWithSphere(otherObject)
 }
