8082421068f220a99302609e0ddcd4f163b3fd41
Jorn van Wier <jornvanwier@gmail.com>
2016-09-22 22:02:07 +0200

    global table

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-let GAME, COLLISIONCONTROLLER, PHYSICSNUMBERS, TESTSPHERE, TESTSPHERE2, CONTROLS, SKYBOX;
+let GAME, COLLISIONCONTROLLER, PHYSICSNUMBERS, TESTSPHERE, POOLTABLE, CONTROLS, SKYBOX;
 
 $(document).ready(function () {
     initThree();
@@ -50,13 +50,13 @@ function initThree() {
     });
     let grassObj = new THREE.Mesh(planeGeom, planeMat);
     GAME.scene.add(grassObj);
-    let pt = new PoolTable(5,8);
+    POOLTABLE = new PoolTable(5,8);
 
     // Create box and ball for collision testing
     // TESTCUBE = new CubeObject(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshPhongMaterial({color: "green"}), true);
     // TESTCUBE.mesh.translateY(10);
     TESTSPHERE = new SphereObject(new THREE.SphereGeometry(0.1, 10, 10), new THREE.MeshPhongMaterial({color: "pink"}), true);
-    TESTSPHERE.mesh.translateY(pt.basePlate.mesh.position.y + pt.basePlate.mesh.geometry.parameters.height / 2 + TESTSPHERE.distanceToGround + 10);
+    TESTSPHERE.mesh.translateY(POOLTABLE.basePlate.mesh.position.y + POOLTABLE.basePlate.mesh.geometry.parameters.height / 2 + TESTSPHERE.distanceToGround + 10);
     // TESTSPHERE2 = new SphereObject(new THREE.SphereGeometry(0.1, 10, 10), new THREE.MeshPhongMaterial({color: "green"}), true);
     // TESTSPHERE2.mesh.translateY(pt.basePlate.mesh.position.y + pt.basePlate.mesh.geometry.parameters.height / 2 + TESTSPHERE.distanceToGround + 20);
     // TESTSPHERE.movement.x = 0.06;
