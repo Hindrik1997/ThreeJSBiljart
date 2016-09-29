66ab274f91c05ef07771853982ee9ab6ec5f03aa
hindrik <hindrik1997@hotmail.com>
2016-09-29 13:37:08 +0200

    temp

--- script/physics/physicsnumbers.js
+++ script/physics/physicsnumbers.js
@@ -1,6 +1,6 @@
 class PhysicsNumbers {
     constructor() {
-        this.gravity = 0.01;
+        this.gravity = 0.0001;
         this.airFriction = 0.95;
         this.floorFriction = 0.98;
     }
